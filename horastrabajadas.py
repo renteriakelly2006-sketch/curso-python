9e66ac1ee7242a3719f50c9418e7fbdb6fd1e453
Kelly Rodríguez Rentería <renteriakelly2006@gmail.com>
2026-05-11 15:01:10 -0600

    un nuevo programa sueldo por horas trabajadas

---
+++ horastrabajadas.py
@@ -0,0 +1,9 @@
+# Pedir los datos al usuario
+horas = float(input("Introduce el número de horas trabajadas: "))
+coste = float(input("Introduce el coste por hora: "))
+
+# Calcular la paga total
+paga = horas * coste
+
+# Mostrar el resultado
+print(f"La paga que te corresponde es {paga}")
